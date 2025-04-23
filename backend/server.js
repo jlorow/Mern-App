28f2cc00c94e02742b930335ad90110940940b76
unknown <johnogweno64@gmail.com>
2025-04-23 13:05:34 +0300

    deploayment changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from "dotenv";
-import { connectDB } from './connfig/db.js';
-import productRoutes from "./routes/product.route.js"
+import { connectDB } from './config/db.js';
+import productRoutes from "./routes/product.route.js";
+import path from "path"
 
 
 dotenv.config();
@@ -9,10 +10,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
-app.use("/api/products", productRoutes)
+app.use("/api/products", productRoutes);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
 app.listen(PORT, () => {
     connectDB();
     console.log('server started at http://localhost' + PORT);
